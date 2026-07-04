7715927bf75e7fcecb77e87b3a9f759326700306
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-04 17:51:38 -0300

    feat: Adicionando o restante de RelatorioRepository

--- Repositories/TotalRepository.cs
+++ Repositories/TotalRepository.cs
@@ -5,18 +5,17 @@ using Dapper;
 using DesafioTecnicoC.Models;
 
 namespace DesafioTecnicoC.Repositories;
-public class TotalRepository{
+public class RelatorioRepository{
 
     private readonly string _connectionString;
-    public TotalRepository(string connectionString)
+    public RelatorioRepository(string connectionString)
     {
         _connectionString = connectionString;
     }
     /// <summary>
-    /// Soma total dos creditos e debitos de cada pessoa.
+    /// Relatório de todas as receitas e despesas, e saldo de cada pessoa.
     /// </summary>
-    public async Task<IEnumerable<RelatorioDespesas>> ListarTodosRelatorioAsync(){
-    
+    public async Task<IEnumerable<RelatorioIndividual?>> ListarRelatoriosPessoalAsync(){
         using var connection = new SqliteConnection(_connectionString);
         string sql = """
           SELECT 
@@ -30,6 +29,22 @@ public class TotalRepository{
           LEFT JOIN transacoes t ON p.id = t.id_pessoa
           GROUP BY p.id, p.nome;
           """;
-        return await connection.QueryAsync<RelatorioDespesas>(sql);
+        return await connection.QueryAsync<RelatorioIndividual>(sql);
+    }
+    
+    /// <summary>
+    /// Relatório geral de todos os totais, de receita, despesa e saldo.
+    /// </summary>
+    public async Task<RelatorioGeral?> ListarRelatorioGeralAsync(){
+      using var connection = new SqliteConnection(_connectionString);
+        string sql = """
+          SELECT 
+              COALESCE(SUM(CASE WHEN tipo = 'CREDITO' THEN valor END), 0) AS total_geral_receitas,
+              COALESCE(SUM(CASE WHEN tipo = 'DEBITO' THEN valor END), 0) AS total_geral_despesas,
+              COALESCE(SUM(CASE WHEN tipo = 'CREDITO' THEN valor END), 0) - 
+              COALESCE(SUM(CASE WHEN tipo = 'DEBITO' THEN valor END), 0) AS saldo_liquido_geral
+          FROM transacoes;
+          """;
+        return await connection.QueryFirstOrDefaultAsync<RelatorioGeral>(sql);
     }
 }
