5fb9c474319a7a881057ed05b0bc476350dcd1bd
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-04 17:37:15 -0300

    feat: Retirando comentários redundantes.

--- migration.sql
+++ migration.sql
@@ -1,14 +1,11 @@
--- Ativa o suporte a Chaves Estrangeiras no SQLite (Obrigatório rodar sempre)
 PRAGMA foreign_keys = ON;
 
--- 1. Tabela de Pessoas
 CREATE TABLE IF NOT EXISTS pessoas (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL UNIQUE,
     idade INTEGER NOT NULL
 );
 
--- 2. Tabela de Transações
 CREATE TABLE IF NOT EXISTS transacoes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     descricao TEXT NOT NULL,
